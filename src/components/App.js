2beb2c51b899453ba521c0a69798511e8fc317d1
SrinivasMuchu <107020456+SrinivasMuchu@users.noreply.github.com>
2022-12-08 10:40:29 +0530

    Button Action - Functional Component

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,13 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const[para,setPara]=useState(false);
+  const[paragraph,setPara]=useState(false);
   const Btn = () =>{
-    setPara(!para)
+    setPara(!paragraph)
   };
   return (
     <div id="main">
-      {para ? <p className="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> : " "}
+      {paragraph ? <p className="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> : ""}
       <button className="click" onClick={Btn}>Paragraph</button>
     </div>
   );
