f9f5f79409c9b3e01c7c5d94e1593ffe552399b4
SrinivasMuchu <107020456+SrinivasMuchu@users.noreply.github.com>
2022-12-08 10:36:04 +0530

    Button Action - Functional Component

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const[para,setPara]=useState(false);
+  const Btn = () =>{
+    setPara(!para)
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      {para ? <p className="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> : " "}
+      <button className="click" onClick={Btn}>Paragraph</button>
     </div>
   );
 }
