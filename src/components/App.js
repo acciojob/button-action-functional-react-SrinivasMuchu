48aa29688afa359c454fac21e34c43775af9a29a
SrinivasMuchu <107020456+SrinivasMuchu@users.noreply.github.com>
2023-01-23 22:21:18 +0530

    donew

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,15 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const[paragraph,setPara]=useState(false);
-  const Btn = () =>{
-    setPara(!paragraph)
-  };
+  const [para,setpara]=useState("");
+  const handleclick=()=>{
+     setpara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+  }
   return (
     <div id="main">
-      {paragraph ?  "":<p className="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
-      <button className="click" onClick={Btn}>Paragraph</button>
+      <button type='submit' classname='btn' id="click" onClick={handleclick}>click</button>
+      // Do not alter the main div
+      <p id='para'>{para}</p>
     </div>
   );
 }
